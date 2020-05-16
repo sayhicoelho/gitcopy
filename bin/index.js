1ce6beb91a30840e19f03dde043062ca6b68a49d
Renan Coelho <sayhicoelho@gmail.com>
2020-05-16 19:35:35 -0300

    Remove .gitcopy before init

--- bin/index.js
+++ bin/index.js
@@ -5,40 +5,42 @@ const path = require('path')
 const commitHashFrom = process.argv[2]
 const commitHashTo = process.argv[3]
 
-exec('git log --oneline', (err, stdout, stderr) => {
-  if (err) {
-    console.error(err)
-  } else if (stderr) {
-    console.error(stderr)
-  } else {
-    const commits = stdout.split('\n')
-    const commitHash = commitHashFrom && commitHashTo
-      ? `${commitHashFrom} ${commitHashTo}`
-      : commits[1].substr(0, 7)
+fs.rmdir('./.gitcopy', { recursive: true }, () => {
+  exec('git log --oneline', (err, stdout, stderr) => {
+    if (err) {
+      console.error(err)
+    } else if (stderr) {
+      console.error(stderr)
+    } else {
+      const commits = stdout.split('\n')
+      const commitHash = commitHashFrom && commitHashTo
+        ? `${commitHashFrom} ${commitHashTo}`
+        : commits[1].substr(0, 7)
 
-    exec(`git diff ${commitHash} --name-only`, (err, stdout, stderr) => {
-      if (err) {
-        console.error(err)
-      } else if (stderr) {
-        console.error(stderr)
-      } else {
-        const files = stdout.split('\n').filter(f => f)
+      exec(`git diff ${commitHash} --name-only`, (err, stdout, stderr) => {
+        if (err) {
+          console.error(err)
+        } else if (stderr) {
+          console.error(stderr)
+        } else {
+          const files = stdout.split('\n').filter(f => f)
 
-        for (const file of files) {
-          const fileName = path.basename(file)
-          const fileDir = path.dirname(file)
+          for (const file of files) {
+            const fileName = path.basename(file)
+            const fileDir = path.dirname(file)
 
-          fs.mkdir(`./.gitcopy/${fileDir}`, { recursive: true }, err => {
-            if (err) {
-              console.error(err)
-            } else {
-              fs.copyFile(`./${file}`, `./.gitcopy/${fileDir}/${fileName}`, () => {
-                console.log(`.gitcopy/${fileDir}/${fileName}`)
-              })
-            }
-          })
+            fs.mkdir(`./.gitcopy/${fileDir}`, { recursive: true }, err => {
+              if (err) {
+                console.error(err)
+              } else {
+                fs.copyFile(`./${file}`, `./.gitcopy/${fileDir}/${fileName}`, () => {
+                  console.log(`.gitcopy/${fileDir}/${fileName}`)
+                })
+              }
+            })
+          }
         }
-      }
-    })
-  }
+      })
+    }
+  })
 })
